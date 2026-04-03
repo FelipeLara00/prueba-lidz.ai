4ecb130f438a9b21eba682b51057ac42bdc427fb
Felipe Lara <felipe.lara@uc.cl>
2026-04-03 17:42:40 -0300

    fix: update apiBaseUrl in nuxt.config.ts to prioritize NUXT_PUBLIC_API_BASE_URL

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -14,7 +14,10 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.BACKEND_URL || 'http://localhost:3000',
+      apiBaseUrl:
+        process.env.NUXT_PUBLIC_API_BASE_URL ||
+        process.env.BACKEND_URL ||
+        'http://localhost:3001',
     },
   },
 
